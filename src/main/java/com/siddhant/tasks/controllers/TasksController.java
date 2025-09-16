d55bb09143404fecc9f0311cb0dc142046cf04bd
Siddhant kandi <kandisiddhant@gmail.com>
2025-09-16 11:23:35 +0530

    Added the functionality of get, delete, update TaskList as well as create Task

---
+++ src/main/java/com/siddhant/tasks/controllers/TasksController.java
@@ -0,0 +1,43 @@
+package com.siddhant.tasks.controllers;
+
+
+import com.siddhant.tasks.domain.dto.TaskDto;
+import com.siddhant.tasks.domain.entities.Task;
+import com.siddhant.tasks.mappers.TaskMapper;
+import com.siddhant.tasks.services.TaskService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping(path = "/task-lists/{task_list_id}/tasks")
+public class TasksController {
+
+    private final TaskService taskService;
+    private final TaskMapper taskMapper;
+
+    public TasksController(TaskService taskService, TaskMapper taskMapper) {
+        this.taskService = taskService;
+        this.taskMapper = taskMapper;
+    }
+
+    @GetMapping
+    public List<TaskDto> listTasks(@PathVariable("task_list_id") UUID taskListId) {
+        return taskService.listTasks(taskListId)
+                .stream()
+                .map(taskMapper::toDto)
+                .toList();
+    }
+
+    @PostMapping
+    public TaskDto createTask(
+            @PathVariable("task_list_id") UUID taskListId,
+            @RequestBody TaskDto taskDto){
+        Task createdTask = taskService.createTask(
+                taskListId,
+                taskMapper.fromDto(taskDto)
+        );
+        return taskMapper.toDto(createdTask);
+    }
+}
